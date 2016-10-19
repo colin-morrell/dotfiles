e0df86643b1138edf246381d903648dbeff0ae99
Colin Morrell <colin@morrell.ninja>
2016-10-19 12:27:07 -0400

    add custom bash config

---
+++ custom/.morrell_mods.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export PATH=$PATH:~/custom
+
+alias ls="ls -G"
+alias pdw="pwd"
+alias json="jq '.'"
+alias tac='tail -r'
+alias updatedb="sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist"
+alias whattime='python ~/whattime.py'
+alias honeycomb='python ~/honeycomb.py'
+
+
+function ipgrab()
+{
+read line; echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}';
+while read line; do echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'; done
+echo $line | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}';
+}
+
+
+shrug(){ echo -n "¯\_(ツ)_/¯" | (pbcopy);echo "¯\_(ツ)_/¯ copied to your clipboard"; }
+
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=50000
+HISTFILESIZE=500000
+HISTCONTROL=ignoreboth
+HISTIGNORE='ls:history'
+PROMPT_COMMAND='history -a'
+
+
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    . $(brew --prefix)/etc/bash_completion
+  fi
